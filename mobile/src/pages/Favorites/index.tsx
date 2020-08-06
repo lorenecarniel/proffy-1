3885b8d051b5a7d66c41c023abe1960b0a8fc748
João Vitor <jv.zaniolo@gmail.com>
2020-08-06 15:31:34 -0300

    finished fourth day

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './styles';
 import { View } from 'react-native';
+import PageHeader from '../../components/PageHeader';
 
 function Favorites() {
-	return <View style={styles.container} />;
+	return (
+		<View style={styles.container}>
+			<PageHeader title='Meus proffys favoritos' />
+		</View>
+	);
 }
 
 export default Favorites;
