92c059499b0c33fcf9d3a948aebcefe8131a661e
João Vitor <jv.zaniolo@gmail.com>
2020-08-08 15:05:36 -0300

    update: routes with SignIn / ForgotPassword

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -4,11 +4,15 @@ import Landing from './pages/Landing';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
 import { BrowserRouter, Route } from 'react-router-dom';
+import ForgotPassword from './pages/ForgotPassword';
+import SignIn from './pages/SignIn';
 
 function Routes() {
 	return (
 		<BrowserRouter>
 			<Route exact path='/' component={Login} />
+			<Route path='/forgotPassword' component={ForgotPassword} />
+			<Route path='/signIn' component={SignIn} />
 			<Route path='/landing' component={Landing} />
 			<Route path='/study' component={TeacherList} />
 			<Route path='/give-classes' component={TeacherForm} />
